df9bf6e5e5c8ac1ecf3e6e27f3371c80cbe0b33c
sashakukhtyk <sashakukhtyk@users.noreply.github.com>
2024-04-15 17:46:50 +0300

    make static look good

--- final_project/settings.py
+++ final_project/settings.py
@@ -116,12 +116,13 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
-
 STATIC_URL = '/static/'
+
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static')
+    os.path.join(BASE_DIR, 'assets'),
+    '/Users/oleksandrkukhtyk/Library/Mobile Documents/com~apple~CloudDocs/final project cs50/final_project/assets',
 ]
-STATIC_ROOT = os.path.join(BASE_DIR, 'assets')
+
 
 MEDIA_URL = 'media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
